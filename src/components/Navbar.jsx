13c4e37058aad6c88c3f83bf3e1e4315cdcecd89
Lok <kurubapradeep44@gmail.com>
2026-02-23 12:35:40 +0530

    Updated Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,61 @@
-import { Link } from 'react-router'
+import { NavLink, Link } from "react-router";
 
 function Navbar() {
   return (
-    <div className="w-full max-w-285 mx-auto ">
-        <div className='flex justify-between p-8'>
-                <Link to='/'>
-                  <h1 className='text-lg font-semibold '>Github Profile Visualizer</h1>
-                </Link>
-                <div>
-                    <ul className='flex space-x-6 text-slate-300 '>
-                        <li className=' hover:text-[#3B82F6] transition-colors duration-200'><Link to='/'>Home</Link></li>
-                        <li className=' hover:text-[#3B82F6] transition-colors duration-200'><Link to='/repositories'>Repositories</Link></li>
-                        <li className=' hover:text-[#3B82F6] transition-colors duration-200'><Link to='/analysis'>Analysis</Link></li>
-                    </ul>
-                </div>
-            </div>
+    <div className="w-full max-w-285 mx-auto">
+      <div className="flex justify-between p-8">
+
+        <Link to="/">
+          <h1 className="text-lg font-semibold">
+            Github Profile Visualizer
+          </h1>
+        </Link>
+
+        <ul className="flex space-x-6 text-slate-300">
+
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                `hover:text-[#3B82F6] ${
+                  isActive ? "text-[#3B82F6]" : "text-[#F8FAFC]"
+                } transition-colors duration-200`
+              }
+            >
+              Home
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink
+              to="/repositories"
+              className={({ isActive }) =>
+                `hover:text-[#3B82F6] ${
+                  isActive ? "text-[#3B82F6]" : "text-[#F8FAFC]"
+                } transition-colors duration-200`
+              }
+            >
+              Repositories
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink
+              to="/analysis"
+              className={({ isActive }) =>
+                `hover:text-[#3B82F6] ${
+                  isActive ? "text-[#3B82F6]" : "text-[#F8FAFC]"
+                } transition-colors duration-200`
+              }
+            >
+              Analysis
+            </NavLink>
+          </li>
+
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
